df0d0df4abc7f8e25affecc9d8be477956fec0f1
Pierre-Jean Vardanéga <pierrejean.vardanega@gmail.com>
2013-01-13 22:11:08 +0100

    Update bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,16 +6,15 @@ class BootStrap {
 
     def init = { servletContext ->
 
-        development {
-            def roleAdmin = Role.findByAuthority("ROLE_ADMIN") ?: new Role(authority: "ROLE_ADMIN").save(true)
-            def roleUser = Role.findByAuthority("ROLE_USER") ?: new Role(authority: "ROLE_USER").save(true)
+        def roleAdmin = Role.findByAuthority("ROLE_ADMIN") ?: new Role(authority: "ROLE_ADMIN").save(true)
+        def roleUser = Role.findByAuthority("ROLE_USER") ?: new Role(authority: "ROLE_USER").save(true)
 
-            def admin = new User(username: "pvardanega", password: "password", enabled: true).save(true)
-            def user = new User(username: "toto", password: "test", enabled: true).save(true)
+        def admin = new User(username: "pvardanega", password: "password", enabled: true).save(true)
+        def user = new User(username: "toto", password: "test", enabled: true).save(true)
 
-            UserRole.create(admin, roleAdmin, true)
-            UserRole.create(user, roleUser, true)
-        }
+        UserRole.create(admin, roleAdmin, true)
+        UserRole.create(user, roleUser, true)
+
     }
     def destroy = {
     }

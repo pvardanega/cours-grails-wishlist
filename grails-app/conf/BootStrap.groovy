5f9757f3e497a8025c0a6f0831a0924ab82a3d8e
Pierre-Jean Vardanéga <pierrejean.vardanega@gmail.com>
2013-01-13 20:18:59 +0100

    Add 2 roles and 2 users granted with one role each

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,19 @@
+import com.pvardanega.Role
+import com.pvardanega.User
+import com.pvardanega.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        def roleAdmin = Role.findByAuthority("ROLE_ADMIN") ?: new Role(authority: "ROLE_ADMIN").save(true)
+        def roleUser = Role.findByAuthority("ROLE_USER") ?: new Role(authority: "ROLE_USER").save(true)
+
+        def admin = new User(username: "pvardanega", password: "password", enabled: true).save(true)
+        def user = new User(username: "toto", password: "test", enabled: true).save(true)
+
+        UserRole.create(admin, roleAdmin, true)
+        UserRole.create(user, roleUser, true)
     }
     def destroy = {
     }

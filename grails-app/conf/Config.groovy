1c5191b0d2f1713f807df5ca3a2b45d02d06e771
Pierre-Jean Vardanéga <pierrejean.vardanega@gmail.com>
2013-01-13 20:11:51 +0100

    Add spring-security-core plugin

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -89,3 +89,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.pvardanega.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.pvardanega.UserRole'
+grails.plugins.springsecurity.authority.className = 'com.pvardanega.Role'
